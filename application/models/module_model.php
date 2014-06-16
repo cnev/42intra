a8f024f16e7db0e19937b82166fe590b5dc8ef2a
Victor JUNG <vjung@e2r2p3.42.fr>
2014-06-16 15:10:44 +0200

    form create

--- application/models/module_model.php
+++ application/models/module_model.php
@@ -18,8 +18,23 @@ class Module_model extends CI_Model
 	public function generate_form()
 	{
 		$str = '<div id="form"><form action="" method="POST">';
-		$str = '<div id="nom">' . '</div>';
-		$str = '</div>';
+		$str .= '<div>nom</div>';
+		$str .= '<input type="text" name="nom">';
+		$str .= '<div> description</div>';
+		$str .= '<textarea id="descr" class="textfield" name="description" rows=10 cols=50></textarea>';
+		$str .= '<div> places</div>';
+		$str .= '<input type="text" name="places" >';
+		$str .= '<div> debut inscription</div>';
+		$str .= '<input type="date" name="debut_inscription">';
+		$str .= '<div> fin inscription</div>';
+		$str .= '<input type="date" name="fin_inscription">';
+		$str .= '<div> debut module</div>';
+		$str .= '<input type="date" name="debut_module">';
+		$str .= '<div> fin module</div>';
+		$str .= '<input type="date" name="fin_module">';
+		$str .= '<div> credits</div>';
+		$str .= '<input type="text" name="credits">';
+		$str .= '</div>';
 		return($str);
 	}
 
